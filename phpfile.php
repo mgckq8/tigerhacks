45a8e7b73f7d9529ed2b62b6b868fd11f8e2d251
Shawhan <jrs48t@umsystem.edu>
2021-11-06 18:34:16 -0500

    added form and php

---
+++ phpfile.php
@@ -0,0 +1,17 @@
+$answer1 = $_POST['fruitQuestion'];
+$answer2 = $_POST['milkQuestion'];
+$answer3 = $_POST['fatQuestion'];
+$answer4 = $_POST['grainQuestion'];
+$answer5 = $_POST['veggieQuestion'];
+$answer5 = $_POST['meatQuestion'];
+
+$totalCorrect = 0;
+
+if ($answer1 == fruit") { $totalCorrect++; }
+if ($answer2 == "cow") { $totalCorrect++; }
+if ($answer3 == "poly") { $totalCorrect++; }
+if ($answer4 == "salt") { $totalCorrect++; }
+if ($answer5 == "leaves") { $totalCorrect++; }
+if ($answer6 == "pork") { $totalCorrect++; }
+
+echo <p>$totalCorrect / 5 correct<p>
