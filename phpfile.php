3007246d97beab9da96e84b7fe0c752de8dee0de
mgckq8 <mgckq8@mail.missouri.edu>
2021-11-06 21:22:33 -0500

    updated php to do gifs

--- phpfile.php
+++ phpfile.php
@@ -3,8 +3,19 @@
     <head>
         <title>SA Function</title>
         <meta charset="utf-8">
+        
+        <script>
+        
+            a {
+                color: white;
+            }
+            
+            h1 {
+                color: white;
+            }
+        </script>
     </head>
-    <body>
+    <body style = "background-image: url('imgs/spaceBackground.jpg')">
 
         <?php
 
@@ -13,7 +24,8 @@
             $answer3 = $_POST['fatQuestion'];
             $answer4 = $_POST['grainQuestion'];
             $answer5 = $_POST['veggieQuestion'];
-            $answer5 = $_POST['meatQuestion'];
+            $answer6 = $_POST['meatQuestion'];
+
 
             $totalCorrect = 0;
 
@@ -23,9 +35,22 @@
             if ($answer4 == "salt") { $totalCorrect++; }
             if ($answer5 == "leaves") { $totalCorrect++; }
             if ($answer6 == "pork") { $totalCorrect++; }
+        
+            if ($totalCorrect < 2) {
+                $outputGif = "meat2.gif";
+            } 
+            if ($totalCorrect < 5 && $totalCorrect > 2) {
+                $outputGif = "veggie.gif";
+            }
+            if ($totalCorrect >= 5) {
+                $outputGif = "watermelon.gif";
+            }
 
-            echo "<a href='index.html'>Back</a>";
-            echo '<h1>' . $totalCorrect . '/5 correct</h1>';
+            echo "<a style='color: white' href='index.html'>Back</a>";
+            echo '<h1 style="color: white">' . $totalCorrect . '/6 correct</h1>';
+            
+            echo '<div class="gifs"><img src="imgs/' . $outputGif .'" alt="This is a funny meat gif" /></div>';
+        
                 
         ?>
     </body>
