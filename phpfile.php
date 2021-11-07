c4f8cb1076e612cd6de578db96c443e7278cff95
mgckq8 <mgckq8@mail.missouri.edu>
2021-11-06 20:56:40 -0500

    fixed up php

--- phpfile.php
+++ phpfile.php
@@ -1,17 +1,32 @@
-$answer1 = $_POST['fruitQuestion'];
-$answer2 = $_POST['milkQuestion'];
-$answer3 = $_POST['fatQuestion'];
-$answer4 = $_POST['grainQuestion'];
-$answer5 = $_POST['veggieQuestion'];
-$answer5 = $_POST['meatQuestion'];
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>SA Function</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
 
-$totalCorrect = 0;
+        <?php
 
-if ($answer1 == fruit") { $totalCorrect++; }
-if ($answer2 == "cow") { $totalCorrect++; }
-if ($answer3 == "poly") { $totalCorrect++; }
-if ($answer4 == "salt") { $totalCorrect++; }
-if ($answer5 == "leaves") { $totalCorrect++; }
-if ($answer6 == "pork") { $totalCorrect++; }
+            $answer1 = $_POST['fruitQuestion'];
+            $answer2 = $_POST['milkQuestion'];
+            $answer3 = $_POST['fatQuestion'];
+            $answer4 = $_POST['grainQuestion'];
+            $answer5 = $_POST['veggieQuestion'];
+            $answer5 = $_POST['meatQuestion'];
 
-echo <p>$totalCorrect / 5 correct<p>
+            $totalCorrect = 0;
+
+            if ($answer1 == "fruit") { $totalCorrect++; }
+            if ($answer2 == "cow") { $totalCorrect++; }
+            if ($answer3 == "poly") { $totalCorrect++; }
+            if ($answer4 == "salt") { $totalCorrect++; }
+            if ($answer5 == "leaves") { $totalCorrect++; }
+            if ($answer6 == "pork") { $totalCorrect++; }
+
+            echo "<a href='index.html'>Back</a>";
+            echo '<h1>' . $totalCorrect . '/5 correct</h1>';
+                
+        ?>
+    </body>
+</html>
